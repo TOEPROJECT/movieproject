19bfdba846ad11e0e0b61143993ab4c4c2173dae
sshh0719 <hansh011@naver.com>
2022-01-05 11:32:46 +0900

    상희_좌석선택

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,12 @@
-
 import React, { useMemo, DatePickerComponent, useState, useEffect } from "react";
 import Select from "react-select";
 import DatePicker from "react-datepicker";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import "./App.css";
+
 import axios from 'axios';
 import Seat from './components/Seat';
 import Pay from './Pay';
-
 
 
 const MovieList = () => {
@@ -39,19 +38,22 @@ const MovieList = () => {
   )
 }
 
-
 function App() {
   return (
     <div className="App">
 
       <BrowserRouter>
         <Routes>
+          {/* 한빛오빠 메인페이지 js로 변환하면 넣기 */}
+          {/* 경완오빠 로그인 페이지 넣기 */}
           <Route path="/" element={<MovieList />}></Route>
+          <Route path = "/seat" element={<Pay/>,<Seat/>}></Route>
+
+          {/* 마이페이지 안에 들어가는 내용 만들어 넣기 */}
+          {/* 덕관오빠 뉴스페이지 넣기 */}
         </Routes>
       </BrowserRouter>
 
-      <Seat></Seat>
-      <Pay></Pay>
 
     </div>
   )
